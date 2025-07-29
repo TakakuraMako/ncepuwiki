c1552a3225f709173ebaa6c400e2dee881d40740
TakakuraMako <1361942776@qq.com>
2025-07-29 22:27:29 +0800

    upgrade

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -9,8 +9,8 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 export default defineNavbarConfig([
   { text: '首页', link: '/' },
   { text: 'WIKI', link: '/wiki/' },
-  { text: '标签', link: '/blog/tags/' },
-  { text: '归档', link: '/blog/archives/' },
+  { text: '标签', link: '/wiki/tags/' },
+  { text: '归档', link: '/wiki/archives/' },
   {
     text: '笔记',
     items: [{ text: '示例', link: '/notes/demo/README.md' }]
--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -35,9 +35,9 @@ export default defineThemeConfig({
    */
   // copyright: true,
 
-  // prevPage: true,   // 是否启用上一页链接
-  // nextPage: true,   // 是否启用下一页链接
-  // createTime: true, // 是否显示文章创建时间
+  prevPage: true,   // 是否启用上一页链接
+  nextPage: true,   // 是否启用下一页链接
+  createTime: true, // 是否显示文章创建时间
 
   /* 站点页脚 */
   // footer: {
@@ -72,10 +72,10 @@ export default defineThemeConfig({
   // },
 
   /* 过渡动画 @see https://theme-plume.vuejs.press/config/basic/#transition */
-  // transition: {
-  //   page: true,        // 启用 页面间跳转过渡动画
-  //   postList: true,    // 启用 博客文章列表过渡动画
-  //   appearance: 'fade',  // 启用 深色模式切换过渡动画, 或配置过渡动画类型
-  // },
+  transition: {
+    page: true,        // 启用 页面间跳转过渡动画
+    postList: true,    // 启用 博客文章列表过渡动画
+    appearance: 'fade',  // 启用 深色模式切换过渡动画, 或配置过渡动画类型
+  },
 
 })
